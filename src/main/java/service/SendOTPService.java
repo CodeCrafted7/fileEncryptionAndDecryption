9d2dae98364ba762b47bf6e1e287e93e4b730387
Shaishav <shaishavgupta7890@gmail.com>
2024-11-19 12:10:01 +0530

    Final commit

---
+++ src/main/java/service/SendOTPService.java
@@ -0,0 +1,36 @@
+package service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendOTPService {
+    public static void sendOTP(String email, String genOTP) {
+        String to = email;
+        String from = "shaishavgupta598@gmail.com";
+        String host = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, "ekyjqbqhfcnklrtc");
+            }
+        });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setText("Your One-time Password for File Enc app is " + genOTP);
+            Transport.send(message);
+            System.out.println("Email sent successfully");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
